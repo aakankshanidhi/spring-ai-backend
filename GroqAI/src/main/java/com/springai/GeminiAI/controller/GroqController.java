5845ed8eda4a8b3bb4025ad116de05a59698863c
Aakanksha Nidhi <akankshanidhi0211@email.com>
2026-05-26 16:16:04 +0530

    added prompt template

--- GroqAI/src/main/java/com/springai/GeminiAI/controller/GroqController.java
+++ GroqAI/src/main/java/com/springai/GeminiAI/controller/GroqController.java
@@ -4,10 +4,11 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
@@ -33,5 +34,33 @@ public class GroqController {
                 .user(question)
                 .call()
                 .content();
+    }
+
+    @PostMapping("/api/recommend")
+    public String recommend(@RequestParam String type, @RequestParam String year, @RequestParam String lang) {
+
+        String tempt = """
+                    I want to watch a {type} movie tonight with good rating,
+                    looking for movies around this year {year}.
+                    The language I'm looking for is {lang}.
+                    Suggest one specific movie and tell me the cast and length of the movie.
+                    
+                    Response format should be:
+                    1.Movie name
+                    2.Basic plot
+                    3.Cast
+                    4.Length
+                    5.IMDB rating 
+                """;
+
+        PromptTemplate promptTemplate = new PromptTemplate(tempt);
+        Prompt prompt = promptTemplate.create(Map.of("type", type, "year", year, "lang", lang));
+
+        String response = chatClient
+                .prompt(prompt)
+                .call()
+                .content();
+
+        return response;
     }
 }
